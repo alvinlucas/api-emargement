93475e2b31c65b55fadedfd0da2eb21c5b3cf859
alvinlucas <84456349+alvinlucas@users.noreply.github.com>
2025-01-14 13:34:42 +0100

    probleme token

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,18 +2,21 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const authMiddleware = (req, res, next) => {
-    const token = req.headers['authorization'];
-    if (!token) {
-        return res.status(401).json({ message: 'Accès non autorisé' });
+    const authHeader = req.headers['authorization'];
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'Accès non autorisé ou mal formaté' });
     }
 
     try {
+        // Extraire uniquement le token après "Bearer"
+        const token = authHeader.split(' ')[1];
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded; // Ajoute les informations utilisateur au req
-        next(); // Continue l'exécution des routes
+        next(); // Passe à la route suivante
     } catch (error) {
         res.status(401).json({ message: 'Token invalide' });
     }
 };
 
 module.exports = authMiddleware;
+
